3d5cbb3b75d43c3e6749371e5e8e9ef6e76d1597
Carlos Penaloza <c.penaloza@uniandes.edu.co>
2017-05-29 16:30:26 -0500

    Arreglos lectura

--- ProyectoDalgo/src/Main.java
+++ ProyectoDalgo/src/Main.java
@@ -1,15 +1,8 @@
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 
 /**
@@ -28,42 +21,41 @@ public class Main {
 	static String primeros = "";
 	static String letras = "";
 	static boolean resuelto = false;
-
+	static boolean prim = false;
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws IOException {
-		Reader r = new Reader();
+	public static void main(String[] args) {
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String str = null;
+			while((str=br.readLine())!=null){
+				//if (!prim)System.out.println();prim=true;
+				String[] input = str.split("[\\=]");
+				String iz = input[0];
 
+				String[] izq = procesarInput(iz,IZQUIERDA);
 
-		// Comienzo solucion -------------------------------------------------------
-		String s;
-		while((s=r.readLine())!=null){
-			s=s.substring(0,s.length()-1);
-			String[] input = s.split("[\\=]");
-			String iz = input[0];
+				String de = input [1];
+				String[] der = procesarInput(de,DERECHA);
 
-			String[] izq = procesarInput(iz,IZQUIERDA);
+				String[] elems = new String[2];
+				elems[0]=izq[0]+der[0];
+				elems[1]=izq[1]+der[1];
 
-			String de = input [1];
-			String[] der = procesarInput(de.substring(1, de.length()),DERECHA);
+				letras=letrasUnicas(izq, der);
+				firmas=calcularFirmas(letras, elems);
+				primeros=primerosCaracteres(izq, der);
 
-			String[] elems = new String[2];
-			elems[0]=izq[0]+der[0];
-			elems[1]=izq[1]+der[1];
-
-			letras=letrasUnicas(izq, der);
-			firmas=calcularFirmas(letras, elems);
-			primeros=primerosCaracteres(izq, der);
-
-			solucionar();
-			if(!resuelto) System.out.println("**********");
-			resuelto=false;
+				solucionar();
+				if(!resuelto) System.out.println("**********");
+				resuelto=false;
+			}
 		}
-
-		// Fin solución ------------------------------------------------------------
-		out.close();
+		catch (Exception e){
+			
+		}
 	}
 
 
@@ -105,7 +97,7 @@ public class Main {
 		resp[0]="";resp[1]="";
 
 		if (input.contains("+")||input.contains("-")){
-			String[] partes =input.split("[\\s+-]+");
+			String[] partes =input.trim().split("[\\s+-]+");
 
 			boolean[] signos = new boolean[partes.length-1];
 
@@ -155,8 +147,8 @@ public class Main {
 
 		}
 		else{
-			if (lado==IZQUIERDA) resp[0] = input;
-			else resp[1] = input;
+			if (lado==IZQUIERDA) resp[0] = input.trim();
+			else resp[1] = input.trim();
 		}
 		return resp;
 	}
@@ -251,174 +243,4 @@ public class Main {
 		}
 		return resp;
 	}
-
-	//-----------PrintWriter para output más rápido---------------------------------
-	public static PrintWriter out;
-
-	//-----------MyScanner class para input más rápido------------------------------
-	public static class MyScanner {
-		BufferedReader br;
-		StringTokenizer st;
-
-		public MyScanner() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-
-		String nextLine(){
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-
-	}
-	//--------------------------------------------------------
-
-	static class Reader
-	{
-		final private int BUFFER_SIZE = 1 << 16;
-		private DataInputStream din;
-		private byte[] buffer;
-		private int bufferPointer, bytesRead;
-
-		public Reader()
-		{
-			din = new DataInputStream(System.in);
-			buffer = new byte[BUFFER_SIZE];
-			bufferPointer = bytesRead = 0;
-		}
-
-		public Reader(String file_name) throws IOException
-		{
-			din = new DataInputStream(new FileInputStream(file_name));
-			buffer = new byte[BUFFER_SIZE];
-			bufferPointer = bytesRead = 0;
-		}
-
-		public String readLine() throws IOException
-		{
-			byte[] buf = new byte[64]; // line length
-			int cnt = 0, c;
-			while ((c = read()) != -1)
-			{
-				if (c == '\n')
-					break;
-				buf[cnt++] = (byte) c;
-			}
-			return new String(buf, 0, cnt);
-		}
-
-		public int nextInt() throws IOException
-		{
-			int ret = 0;
-			byte c = read();
-			while (c <= ' ')
-				c = read();
-			boolean neg = (c == '-');
-			if (neg)
-				c = read();
-			do
-			{
-				ret = ret * 10 + c - '0';
-			}  while ((c = read()) >= '0' && c <= '9');
-
-			if (neg)
-				return -ret;
-			return ret;
-		}
-
-		public long nextLong() throws IOException
-		{
-			long ret = 0;
-			byte c = read();
-			while (c <= ' ')
-				c = read();
-			boolean neg = (c == '-');
-			if (neg)
-				c = read();
-			do {
-				ret = ret * 10 + c - '0';
-			}
-			while ((c = read()) >= '0' && c <= '9');
-			if (neg)
-				return -ret;
-			return ret;
-		}
-
-		public double nextDouble() throws IOException
-		{
-			double ret = 0, div = 1;
-			byte c = read();
-			while (c <= ' ')
-				c = read();
-			boolean neg = (c == '-');
-			if (neg)
-				c = read();
-
-			do {
-				ret = ret * 10 + c - '0';
-			}
-			while ((c = read()) >= '0' && c <= '9');
-
-			if (c == '.')
-			{
-				while ((c = read()) >= '0' && c <= '9')
-				{
-					ret += (c - '0') / (div *= 10);
-				}
-			}
-
-			if (neg)
-				return -ret;
-			return ret;
-		}
-
-		private void fillBuffer() throws IOException
-		{
-			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
-			if (bytesRead == -1)
-				buffer[0] = -1;
-		}
-
-		private byte read() throws IOException
-		{
-			if (bufferPointer == bytesRead)
-				fillBuffer();
-			return buffer[bufferPointer++];
-		}
-
-		public void close() throws IOException
-		{
-			if (din == null)
-				return;
-			din.close();
-		}
-	}
-
 }
