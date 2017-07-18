42560ef8329b299d502e7201e3cbcd0b5f6c80ee
Carlos Penaloza <c.penaloza@uniandes.edu.co>
2017-07-17 22:51:20 -0500

    Version final B

--- ProyectoDalgo/src/ProblemaB.java
+++ ProyectoDalgo/src/ProblemaB.java
@@ -3,11 +3,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 /**
- * @author c.penaloza
- * @author jd.gonzaleza
+ * @author c.penaloza Carlos Peñaloza 201531973
+ * @author jd.gonzaleza Juan Diego González 201531418
  */
 
 /**
@@ -18,45 +19,65 @@ public class ProblemaB {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-
+	private static boolean resp[];
+	private static int[] p;
+	private static double respuesta;
+	private static int n;
+	public static void main(String[] args) throws IOException {
+//
 		MyScanner sc = new MyScanner();
 		out = new PrintWriter(new BufferedOutputStream(System.out));
 
 		// Comienzo solucion -------------------------------------------------------
+		resp = new boolean[0];
+		 p = new int[0] ;
+		String s;
 
-		Integer n = Integer.parseInt(sc.nextLine());
-		int[] p = new int[n];
+		LinkedList<String> resultados = new LinkedList<>();
+		while(!(s = sc.nextLine()).equals("")){
+			String[] x = s.split(" ");
+			
+			if(x.length ==1){
+				 n= Integer.parseInt(x[0]);
+				 p = new int[n];
+			}
+			else{
+				for (int i=0; i<x.length; i++){
+					p[i]=Integer.parseInt(x[i]);
+				}
+				resp = new boolean[101];
+				for(int k=0; k<=n-1; k++){
+					for(int l = 100; l>=0; l--){
+						construible(l, k);
+					}
+				}
 
-		for (int i=0; i<n; i++){
-			p[i]=sc.nextInt();
+				for(int i = 51; i<=100; i++){
+					if(resp[i] == true){
+						respuesta = 100*(p[n-1]/(double)i);
+						break;
+					}
+
+
+				}
+				resultados.add(Math.round(respuesta* 100.00)/100.00+"");
+			}
 		}
-		
-		
-
-		out.println();
-
+		for(String r: resultados){
+			out.println(r);
+		}
 		// Fin solución ------------------------------------------------------------
 		out.close();
 	}
 
-	/**
-	 * Predicado verdadero si se puede construir el entero a con las sumas de p1 y pj
-	 * @param a entero estudiado
-	 * @param j indice de p hasta el cual se sumará
-	 * @param p arreglo de participaciones
-	 * @return true si es construible, false de lo contrario
-	 */
-	private static boolean construible (int a, int j, int[] p){
-		int i=0,suma=0;
-		while(i<j){
-			suma+=p[i];
-			if (suma>=a) return true;
-			i++;
-		}
-		return false;
-	}
 
+	private static void construible (int a, int j){
+		if(j==0){
+			resp[a]= (a==p[p.length-1]);
+		}else if(p[(j+p.length-1)%p.length]<=a){
+			resp[a]= resp[a]|| resp[(a-p[(j+p.length-1)%p.length])];
+		}
+	}
 
 	//-----------PrintWriter para output más rápido---------------------------------
 	public static PrintWriter out;
